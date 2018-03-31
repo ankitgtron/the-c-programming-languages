8a7d647046ad34c2cea602039a70ec540c35878c
Ankit Gaur <37956294+ankitgtron@users.noreply.github.com>
2018-03-31 23:48:26 +0530

    Add files via upload

---
+++ exercise1.7.c
@@ -0,0 +1,13 @@
+// program to print the value of the EOF.
+// If you are using linux system enter ctrl + d in terminal for EOF.
+// If on windows system enter ctrl + z in terminal for EOF.
+#include <stdio.h>
+
+int main(void)
+{
+  printf("The value of EOF is %d\n\n", EOF);
+  
+  return 0;
+}
+
+
