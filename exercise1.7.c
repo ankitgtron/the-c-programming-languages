a3be99b500a20f9b9b287e354502cbe71e42d200
Ankit Gaur <37956294+ankitgtron@users.noreply.github.com>
2018-04-01 14:32:38 +0530

    Update exercise1.7.c

--- exercise1.7.c
+++ exercise1.7.c
@@ -1,6 +1,8 @@
-// program to print the value of the EOF.
-// If you are using linux system enter ctrl + d in terminal for EOF.
-// If on windows system enter ctrl + z in terminal for EOF.
+/* program to print the value of the EOF.
+*  If you are using linux system enter ctrl + d in terminal for EOF.
+*  If on windows system enter ctrl + z in terminal for EOF.
+*/
+
 #include <stdio.h>
 
 int main(void)
