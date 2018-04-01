e7a588aa6c321a62b9db3e320d2d6c21efe36dc4
Ankit Gaur <37956294+ankitgtron@users.noreply.github.com>
2018-04-01 22:35:27 +0530

    always do simple

---
+++ exercise1-13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+   int c;
+        
+   while ((c = getchar()) != EOF)
+        {
+           if (c == ' ' || c == '\t' || c == '\n' )
+              putchar('\n');
+
+           else 
+              putchar('-');
+        }
+    return 0;
+} 
