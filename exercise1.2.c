95415ab2ad2509ba26f212162c60dcbaccc5cad5
Ankit Gaur <37956294+ankitgtron@users.noreply.github.com>
2018-04-01 14:29:47 +0530

    Update exercise1.2.c

--- exercise1.2.c
+++ exercise1.2.c
@@ -1,11 +1,11 @@
 /*
-By 'above', the question is referring to:
-\n (newline)
-\t (tab)
-\b (backspace)
-\" (double quote)
-\\ (backslash) 
-We have to tread carefully here, because using a non-specified escape sequence invokes undefined behaviour. The following program attempts to demonstrate all the legal escape sequences, not including the ones already shown (except \n , which I actually need in the program), and not including hexadecimal and octal escape sequences.
+* By 'above', the question is referring to:
+* \n (newline)
+* \t (tab)
+* \b (backspace)
+* \" (double quote)
+* \\ (backslash) 
+* We have to tread carefully here, because using a non-specified escape sequence invokes undefined behaviour. The following program attempts to demonstrate all the legal escape sequences, not including the ones already shown (except \n , which I actually need in the program), and not including hexadecimal and octal escape sequences.
 */
 #include <stdio.h>
 
